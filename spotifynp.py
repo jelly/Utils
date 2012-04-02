43049168433ffd3ddcdbb2fa20be538621004345
Jelle van der Waa <jelle@vdwaa.nl>
2012-04-02 22:16:39 +0200

    A now playing spotify notification daemon
    Signed-off-by: Jelle van der Waa <jelle@vdwaa.nl>

---
+++ spotifynp.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python2
+
+import dbus
+import gobject
+import pynotify
+import urllib
+import gtk
+
+from dbus.mainloop.glib import DBusGMainLoop
+from dbus.exceptions import DBusException
+
+
+class SpotifyNotifier(object):
+
+    def __init__(self):
+        """initialise."""
+        bus_loop = DBusGMainLoop(set_as_default=True)
+        self.bus = dbus.SessionBus(mainloop=bus_loop)
+        loop = gobject.MainLoop()
+        self.notify_id = None
+        try:
+            self.props_changed_listener()
+        except DBusException, e:
+            if not ("org.mpris.MediaPlayer2.spotify "
+                    "was not provided") in e.get_dbus_message():
+                raise
+        self.session_bus = self.bus.get_object("org.freedesktop.DBus",
+                                 "/org/freedesktop/DBus")
+        self.session_bus.connect_to_signal("NameOwnerChanged",
+                                        self.handle_name_owner_changed,
+                                        arg0="org.mpris.MediaPlayer2.spotify")
+
+        loop.run()
+
+    def props_changed_listener(self):
+        """Hook up callback to PropertiesChanged event."""
+        self.spotify = self.bus.get_object("org.mpris.MediaPlayer2.spotify",
+                                           "/org/mpris/MediaPlayer2")
+        self.spotify.connect_to_signal("PropertiesChanged",
+                                        self.handle_properties_changed)
+
+    def handle_name_owner_changed(self, name, older_owner, new_owner):
+        """Introspect the NameOwnerChanged signal to work out if spotify has started."""
+        if name == "org.mpris.MediaPlayer2.spotify":
+            if new_owner:
+                # spotify has been launched - hook it up.
+                self.props_changed_listener()
+            else:
+                self.spotify = None
+
+
+    def handle_properties_changed(self, interface, changed_props, invalidated_props):
+        """Handle track changes."""
+        metadata = changed_props.get("Metadata", {})
+        if metadata:
+            if pynotify.init("Spotify Notifier Demo"):
+
+                title = metadata.get("xesam:title").encode('UTF-8')
+                album = metadata.get("xesam:album").encode('UTF-8')
+                artist = metadata.get("xesam:artist")[0].encode('UTF-8')
+                url = metadata.get('mpris:artUrl') 
+                url = url.replace('thumb','image')
+                f = urllib.urlopen(url)
+                data = f.read()
+                pbl = gtk.gdk.PixbufLoader()
+                pbl.write(data)
+                pbuf = pbl.get_pixbuf()
+                pbl.close()
+
+                alert = pynotify.Notification(title,"By %s from %s" % (artist, album))
+                alert.set_icon_from_pixbuf(pbuf)
+                alert.set_urgency(pynotify.URGENCY_NORMAL)
+                alert.set_timeout(pynotify.EXPIRES_DEFAULT)
+                alert.show()
+
+if __name__ == "__main__":
+    SpotifyNotifier()
